6d41a7350b34fbb8348b134a510f16f6080aa486
sebastianver123 <sebastianver1234@gmail.com>
2017-05-03 18:04:32 -0500

    Add files via upload

---
+++ MicroAnalitica2/src/microanalitica2/Histograma.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package microanalitica2;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Histograma {
+    
+    
+    int R[];
+    int G[];
+    int B[];
+    
+    
+    
+    
+}
---
+++ MicroAnalitica2/src/microanalitica2/Imagen.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package microanalitica2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Imagen {
+    
+    Scanner miEscaner= new Scanner(System.in);
+    
+    Pixel miPixel[][];
+    
+    void ObtenerDatos(){
+        
+        this.miEscaner.
+        
+        
+    }
+    
+    
+}
